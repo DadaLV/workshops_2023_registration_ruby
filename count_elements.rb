a50efe2944e84adbab693f174129de783da48336
DadaLV <vynohradova.l.a@gmail.com>
2023-09-24 16:06:08 +0200

    Add completed tasks

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,13 @@
 class CountElements
+  def initialize(array)
+    @array = array
+  end
 
+  def perform
+    counts = Hash.new(0)
+    @array.each do |element|
+      counts[element] += 1
+    end
+    counts
+  end
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,10 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def perform
+    regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#\$%&*\+=:;?<>]).{6,24}$/
+    regex.match?(@password) && !@password.scan(/(.)\1{2,}/).any?
+  end
 end
